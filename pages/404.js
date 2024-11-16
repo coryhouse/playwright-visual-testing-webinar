81fa22acd9b08de5db754ecc9b6487046566ac17
Cory House <housecor@gmail.com>
2024-11-16 13:36:33 -0600

    fix

--- pages/404.js
+++ pages/404.js
@@ -1,25 +1,27 @@
-import Link from 'next/link';
-import { Header } from '../E-Commerce/components/Header/header';
-import { Footer } from '../E-Commerce/components/footer';
-import SampleButton from '../components/SampleComponents/SampleButton/sampleButton';
+import Link from "next/link";
+import SampleButton from "../components/SampleComponents/SampleButton/sampleButton";
 
 export default function Custom404() {
-	return (
-		<>
-			<Header />
-			<div style={{ textAlign: 'center', marginTop: '50px' }}>
-				<h1>404 - Page Not Found</h1>
-				<p>Oops! The page you are looking for does not exist.</p>
-				<SampleButton
-					style={{backgroundColor: '#00a399', fontWeight: 'bold', padding: '5px 20px', boxShadow: '2px 2px 3px #e1e1e1', marginTop: "30px"}}
-				>
-					<Link href="/e-commerce">
-						Go back to Home
-					</Link>
-				</SampleButton>
-			</div>
+  return (
+    <>
+      <Header />
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>404 - Page Not Found</h1>
+        <p>Oops! The page you are looking for does not exist.</p>
+        <SampleButton
+          style={{
+            backgroundColor: "#00a399",
+            fontWeight: "bold",
+            padding: "5px 20px",
+            boxShadow: "2px 2px 3px #e1e1e1",
+            marginTop: "30px",
+          }}
+        >
+          <Link href="/e-commerce">Go back to Home</Link>
+        </SampleButton>
+      </div>
 
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 }
